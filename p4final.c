5a7193a50768a7da9ad9bf394c83ebec44d83b32
M SANJEEV <98035905+sanjureddysj@users.noreply.github.com>
2022-01-19 16:39:25 +0000

    Executed

---
+++ p4final.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+
+int input()
+{
+  int a;
+  printf("Enter Three Numbers\n");
+  scanf("%d", &a);
+  return a;
+}
+
+int compare(int a, int b, int c)
+{
+  int large;
+  if((a>b)&&(a>c))
+  {
+    large=a;
+  }
+  
+  else if((b>a)&&(b>c))
+  {
+    large=b;
+  }
+  
+  else
+  {
+    large=c;
+  }
+
+  return large;
+
+}
+
+void output(int a,int b,int c,int large)
+{
+  printf("The Largest Number is %d", large);
+}
+
+int main()
+{
+  int a,b,c,large;
+  a=input();
+  b=input();
+  c=input();
+  large=compare(a,b,c);
+  output(a,b,c,large);
+  return 0;
+}
